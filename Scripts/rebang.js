2b88b0d762906b4694af188515fd64fe8c57c8ce
Claus.Lee <phpvalue@yahoo.com>
2023-09-18 16:11:17 +0800

    Update rebang.js

--- Scripts/rebang.js
+++ Scripts/rebang.js
@@ -1,13 +1,9 @@
 var body = JSON.parse($response.body);
 
-body.data.vipInfo = {
-      "error": false,
-      "data": {
-        "vip_expired": "9999999999",
+body.data = {
+       "vip_expired": "9999999999",
         "is_vip_now": true,
         "is_vip": "1"
-      },
-      "status":200
     };
 
 $done({ body: JSON.stringify(body) });
